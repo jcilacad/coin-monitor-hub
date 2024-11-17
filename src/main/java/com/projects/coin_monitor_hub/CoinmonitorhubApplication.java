92fa4c040f2b3113e51a95f35e12d97a8280def7
jcilacad <johnchristopherilacad27@gmail.com>
2024-11-18 01:39:18 +0800

    Initial commit

---
+++ src/main/java/com/projects/coin_monitor_hub/CoinmonitorhubApplication.java
@@ -0,0 +1,13 @@
+package com.projects.coin_monitor_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinmonitorhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinmonitorhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/coin_monitor_hub/CoinmonitorhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.coin_monitor_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinmonitorhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
